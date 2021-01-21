dea68cfb95726f23d8dc6d3627fb626340afdea8
MungC2010G2 <zhaiduong26z@gmail.com>
2021-01-21 09:19:14 +0700

    upgrade hàm thêm mới đối tượng

--- src/model/ManagerContact.java
+++ src/model/ManagerContact.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ManagerContact {
+    List<Contacts> contactsList = new ArrayList<>();
+    public static final String fileName = "contacts.csv";
+
+    public List<Contacts> getContactsList() {
+        return contactsList;
+    }
+
+    public void addNew(Contacts contacts) {
+        contactsList.add(contacts);
+        System.out.println("Thêm thành công");
+    }
+
 }
