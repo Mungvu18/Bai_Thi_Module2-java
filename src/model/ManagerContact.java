af9d90e9c09f518981ae8afe7d383b9a0335e548
MungC2010G2 <zhaiduong26z@gmail.com>
2021-01-21 09:39:47 +0700

    thêm hàm đọc file

--- src/model/ManagerContact.java
+++ src/model/ManagerContact.java
@@ -17,11 +17,13 @@ public class ManagerContact {
         contactsList.add(contacts);
         System.out.println("Thêm thành công");
     }
+
     public void showAll() {
         for (Contacts contacts : contactsList) {
             System.out.println(contacts);
         }
     }
+
     public int edit(String phoneNumber) {
         int index = -1;
         for (int i = 0; i < contactsList.size(); i++) {
@@ -31,6 +33,7 @@ public class ManagerContact {
         }
         return index;
     }
+
     public int remove(String phoneNumber) {
         int index = -1;
         for (int i = 0; i < contactsList.size(); i++) {
@@ -40,6 +43,7 @@ public class ManagerContact {
         }
         return index;
     }
+
     public void findByPhoneNumber(String phoneNumber) {
         boolean check = false;
         for (Contacts contacts : contactsList) {
@@ -52,6 +56,7 @@ public class ManagerContact {
             System.out.println("Không tìm được danh bạ với số điện thoại trên");
         }
     }
+
     public void findByName(String name) {
         boolean check = false;
         for (Contacts contacts : contactsList) {
@@ -64,27 +69,42 @@ public class ManagerContact {
             System.out.println("Không tìm được danh bạ với tên trên");
         }
     }
-    public void readFile(){
-        try{
+
+    public void readFile() {
+        try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
-            while ((line = bufferedReader.readLine()) != null){
+            while ((line = bufferedReader.readLine()) != null) {
                 String[] strings = line.split(",");
-                Contacts contacts = new Contacts(strings[0],strings[1],strings[2],strings[3],strings[4],strings[5],strings[6]);
+                Contacts contacts = new Contacts(strings[0], strings[1], strings[2], strings[3], strings[4], strings[5], strings[6]);
                 contactsList.add(contacts);
             }
             bufferedReader.close();
             fileReader.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
         showAll();
     }
 
-}
+    public void writeFile() {
+        try {
+            FileWriter fileWriter = new FileWriter(fileName);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            String string = "";
+            for (Contacts contacts : contactsList) {
+                string = contacts.getPhoneNumber() + "," + contacts.getGroup() + "," + contacts.getName() + "," + contacts.getGender()
+                        + "," + contacts.getAddress() + "," + contacts.getBirthday() + "," + contacts.getEmail() + "\n";
+                bufferedWriter.write(string);
+            }
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-
+    }
 
 
 }
